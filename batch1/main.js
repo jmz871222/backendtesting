66ee71867aa5c3c73603559e37fa259af992a344
JIANG MINGZHI <jiang.missouri@gmail.com>
2022-02-24 12:23:58 +0800

    Enter

--- batch1/main.js
+++ batch1/main.js
@@ -1,16 +1,43 @@
 const { response } = require("express");
 
-const {router} = require ("./routers");
+const express = require("express");
+
+const database =  require("./data");
+
+let router = express.Router();
+
+router.get("/home", (request, response) => {
+    response.send("Welcome to Dev 2");
+});
+
+
+
+router.get("/sum", (request, response) => {
+    let sum = request.query.a + request.query.b;
+    response.send("sum" + sum);
+});
+
+router.get("/user/all", (request, response) => {
+   let users = database.get_all_users();
+   response.send(users);
+
+});
+
+router.get("/user/by-uid", (request, response) => {
+    let users = database.get_user_by_user_id(request.query.user_id);
+    response.send(users);
+  });
+
+router.get("/user/add", (request, response) => {
+    let user = request.body;
+    database.add_user(user);
+    response.send("User added");
+  });
 
 
 let app = express();
 
 app.use(router);
-
-
-
-
-
 
 //app.get();
 
