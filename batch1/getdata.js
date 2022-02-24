c372d152af8259168d5ff95d63fc76e33b57b1f8
JIANG MINGZHI <jiang.missouri@gmail.com>
2022-02-24 09:51:35 +0800

    Enter

--- batch1/getdata.js
+++ batch1/getdata.js
@@ -1,36 +1,19 @@
-//syntax: $.getJSON(url, [data], [callback]);
-//AJAX library
-// const b1 = document.getElementById("b1");
-// b1.addEventListener("click", getFromServer);
 
-console.log("Hello");
-let names = ["Dixant", "Jon", "Keith"];
 
-let details = {
-  first_name: "Dixant",
-  last_name: "Mittal",
-};
-
-let users = [
-  {
-    first_name: "Dixant",
-    last_name: "Mittal",
-  },
-  {
-    first_name: "Jon",
-    last_name: "Scheele",
-  },
-];
-let all_names = "";
-
-let code = "<ul>";
-
-names.forEach((name) => {
-  code = code + "<li>" + name + "</li>";
+const b1 = document.getElementById("b1");
+b1.addEventListener("click", () => {
+  $.getJSON(
+    // "http://nusbackendstub.herokuapp.com/user/all",
+    "http://nusbackendstub.herokuapp.com/user/by-uid?user_id=1",
+    (data) => {
+      let code = "<ul>";
+      data.forEach((datapoint) => {
+        code += `<li> First Name: ${datapoint.first_name}
+                    Last Name: ${datapoint.last_name}
+                    Email: ${datapoint.email} </li>`;
+      });
+      code += "</ul>";
+      $(".mypanel").html(code);
+    }
+  );
 });
-
-code = code + "</ul>"
-
-$(".mypanel").html(code);
-
-function perform_operation()
